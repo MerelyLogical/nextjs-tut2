5be44c97ec704d763f40f82fcb1e9405c033b7a1
MerelyLogical <qieerbushe7@gmail.com>
2025-08-31 18:20:48 +0100

    enable static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    basePath: '/nextjs-tut',
+    distDir: 'docs',
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
